46c453ca074efa6ad00a11d6b4659d63ca3fd46d
menaaziz27 <menaaziz27@gmail.com>
2021-03-11 20:16:05 +0200

    test

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
 // ============ Node-Packages ============
-const express = require("express");
-const bodyparser = require("body-parser");
-const session = require("express-session");
-const csrf = require("csurf");
-const flash = require("connect-flash");
-const multer = require("multer");
-const MongoDBStore = require("connect-mongodb-session")(session);
-require('ejs')
+const express = require('express');
+const bodyparser = require('body-parser');
+const session = require('express-session');
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const multer = require('multer');
+const MongoDBStore = require('connect-mongodb-session')(session);
+require('ejs');
 
 // ============ Core-Modules ============
-const path = require("path");
+const path = require('path');
 
 // ============ My-Modules ============
-require("./utils/db");
-const { findUser } = require("./middleware/helper");
+require('./utils/db');
+const { findUser } = require('./middleware/helper');
 
 // ============ constant vars ============
 // const MongoDB_URI = 'mongodb+srv://abdallah:abd12345@cluster0.itsjp.mongodb.net/ZeroToOne?&w=majority';
-const MongoDB_URI = "mongodb://localhost:27017/zerotoonee";
+const MongoDB_URI = 'mongodb://localhost:27017/zerotoone';
 
 const app = express();
 
 // Routes
-const homeRoutes = require("./routes/home");
-const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user");
-const postRoutes = require("./routes/post");
+const homeRoutes = require('./routes/home');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const postRoutes = require('./routes/post');
 
 // storing sessions in DB
 const store = new MongoDBStore({
 	uri: MongoDB_URI,
-	collection: "sessions",
+	collection: 'sessions',
 });
 
 // set ejs template engines
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 // app.set('views', 'views');
 
 //set upload image settings
 const fileStorage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, "images");
+		cb(null, 'images');
 	},
 	filename: (req, file, cb) => {
 		cb(null, file.originalname);
@@ -49,9 +49,9 @@ const fileStorage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
 	if (
-		file.mimetype === "image/png" ||
-		file.mimetype === "image/jpg" ||
-		file.mimetype === "image/jpeg"
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg' ||
+		file.mimetype === 'image/jpeg'
 	) {
 		cb(null, true);
 	} else {
@@ -61,15 +61,17 @@ const fileFilter = (req, file, cb) => {
 
 // ==== middlewares which will be executed before every incoming request ====
 app.use(bodyparser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // app.use("/public", express.static("./public/"));
-app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single("image"));
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(
+	multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 // for attaching session object in every request and connect the cookie id with its
 // appropriate user session
 app.use(
 	session({
-		secret: "my secret",
+		secret: 'my secret',
 		resave: false,
 		saveUninitialized: false,
 		store: store,
@@ -90,9 +92,9 @@ app.use((req, res, next) => {
 
 // ============ Routes ============
 app.use(homeRoutes);
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 // app.use('/admin', adminRoutes)
 // app.use(notFoundRoute)
 // app.use((error, req, res, next) => {
