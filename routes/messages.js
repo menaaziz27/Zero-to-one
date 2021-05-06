20f1132ee1a28707fdc8cbfc9d27fb0bc54540d7
menaaziz27 <menaaziz27@gmail.com>
2021-05-06 09:51:21 +0200

    accessing chat

--- routes/messages.js
+++ routes/messages.js
@@ -4,10 +4,12 @@ const router = require('express').Router();
 const {
 	getMessages,
 	newMessage,
+	getChatPage,
 } = require('../controllers/messagesController');
 const { isAuthenticated } = require('../middleware/isAuthenticated');
 
 router.get('/', getMessages);
 router.get('/new', newMessage);
+router.get('/:chatId', getChatPage);
 
 module.exports = router;
