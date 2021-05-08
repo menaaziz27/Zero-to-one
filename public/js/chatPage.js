9d6e551afb1698d1f6bb3d0c20935fa93f01f0a6
menaaziz27 <menaaziz27@gmail.com>
2021-05-08 10:47:37 +0200

    fixed chatPage layout & chat scroll & animation onSendMessage scroll

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -1,137 +1,129 @@
 $(document).ready(() => {
-	$('.inputTextbox').focus();
-	$.get(`/chats/${chatId}`, data => {
-		if (data.users.length == 1) {
-			alert('other user is not available now');
-		}
-		$('#chatName').text(getChatName(data));
-	});
+    $('.inputTextbox').focus();
+    $.get(`/chats/${chatId}`, data => {
+        if (data.users.length == 1) {
+            alert('other user is not available now');
+        }
+        $('#chatName').text(getChatName(data));
+    });
 
-	$.get(`/chats/${chatId}/messages`, data => {
-		var messages = [];
-		let lastSenderId = '';
+    $.get(`/chats/${chatId}/messages`, data => {
+        var messages = [];
+        let lastSenderId = '';
 
-		data.forEach((message, index) => {
-			var html = createMessageHtml(message, data[index + 1], lastSenderId);
-			messages.push(html);
+        data.forEach((message, index) => {
+            var html = createMessageHtml(message, data[index + 1], lastSenderId);
+            messages.push(html);
 
-			lastSenderId = message.sender._id;
-		});
-		var messagesHtml = messages.join('');
-		addMessagesHtmlToPage(messagesHtml);
-	});
+            lastSenderId = message.sender._id;
+        });
+        var messagesHtml = messages.join('');
+        addMessagesHtmlToPage(messagesHtml);
+        scrollToBottom(false);
+    });
 });
 
 $('#chatNameButton').click(() => {
-	let chatName = $('#chatNameTextbox').val().trim();
+    let chatName = $('#chatNameTextbox').val().trim();
 
-	$.ajax({
-		url: '/chats/' + chatId,
-		method: 'PUT',
-		data: { chatName },
-		success: (data, status, xhr) => {
-			if (xhr.status !== 204) {
-				alert('couldnt update name');
-			} else {
-				location.reload();
-			}
-		},
-	});
+    $.ajax({
+        url: '/chats/' + chatId,
+        method: 'PUT',
+        data: { chatName },
+        success: (data, status, xhr) => {
+            if (xhr.status !== 204) {
+                alert('couldnt update name');
+            } else {
+                location.reload();
+            }
+        },
+    });
 });
 
 $('.sendMessageButton').click(() => {
-	messageSubmitted();
+    messageSubmitted();
 });
 $('.inputTextbox').keydown(event => {
-	if (event.which === 13) {
-		messageSubmitted();
-		return false;
-	}
+    if (event.which === 13) {
+        messageSubmitted();
+        return false;
+    }
 });
 
 function messageSubmitted() {
-	var content = $('.inputTextbox').val().trim();
+    var content = $('.inputTextbox').val().trim();
 
-	if (content != '') {
-		sendMessage(content);
-		$('.inputTextbox').val('');
-		$('.inputTextbox').focus();
-	}
+    if (content != '') {
+        sendMessage(content);
+        $('.inputTextbox').val('');
+        $('.inputTextbox').focus();
+    }
 }
 
 function sendMessage(content) {
-	$.post(
-		'/chatMessage',
-		{ content: content, chatId: chatId },
-		(data, status, xhr) => {
-			if (xhr.status != 201) {
-				alert('Could not send message');
-				$('.inputTextbox').val(content);
-				return;
-			}
-			addChatMessageHtml(data);
-		}
-	);
+    $.post(
+        '/chatMessage', { content: content, chatId: chatId },
+        (data, status, xhr) => {
+            if (xhr.status != 201) {
+                alert('Could not send message');
+                $('.inputTextbox').val(content);
+                return;
+            }
+            addChatMessageHtml(data);
+        }
+    );
 }
 
 function addMessagesHtmlToPage(html) {
-	$('.chatMessages').append(html);
+    $('.chatMessages').append(html);
 }
 
 function addChatMessageHtml(message) {
-	if (!message || !message._id) {
-		alert('Message is not valid');
-		return;
-	}
+    if (!message || !message._id) {
+        alert('Message is not valid');
+        return;
+    }
 
-	var messageDiv = createMessageHtml(message, null, '');
+    var messageDiv = createMessageHtml(message, null, '');
 
-	addMessagesHtmlToPage(messageDiv);
+    addMessagesHtmlToPage(messageDiv);
+    scrollToBottom(true);
 }
 
 function createMessageHtml(message, nextMessage, lastSenderId) {
-	console.log(message);
-	let sender = message.sender;
-	let senderName = sender.name;
-	console.log(sender, 'sender');
-	let currentSenderId = sender._id;
-	let nextSenderId = nextMessage !== null ? nextMessage?.sender._id : '';
+    let sender = message.sender;
+    let senderName = sender.name;
+    let currentSenderId = sender._id;
+    let nextSenderId = nextMessage != null ? nextMessage.sender._id : '';
 
-	console.log(`lastSenderId: ${lastSenderId}`);
-	console.log(`currentSenderId: ${currentSenderId}`);
-	let isFirst = lastSenderId !== currentSenderId;
-	let isLast = nextSenderId !== currentSenderId;
-	console.log(`isFirst : ${isFirst}`);
-	console.log(`isLast: ${isLast}`);
-	console.log(`message.sender._id: ${message.sender._id}`);
-	console.log(`userLoggedIn._id: ${userLoggedIn._id}`);
-	let isMine = message.sender._id === userLoggedIn._id;
-	console.log(`isMine: ${isMine}`);
-	let liClassName = isMine ? 'mine' : 'theirs';
+    let isFirst = lastSenderId !== currentSenderId;
+    let isLast = nextSenderId !== currentSenderId;
+    let isMine = message.sender._id === userLoggedIn._id;
+    let liClassName = isMine ? 'mine' : 'theirs';
 
-	let nameElement = '';
-	if (isFirst) {
-		liClassName += ' first';
+    let nameElement = '';
+    if (isFirst) {
+        liClassName += ' first';
 
-		if (!isMine) {
-			nameElement = `<span class="senderName">${senderName}</span>`;
-		}
-	}
+        if (!isMine) {
+            nameElement = `<span class="senderName">${senderName}</span>`;
+        }
+    }
 
-	let profileImage = '';
-	if (isLast) {
-		liClassName += ' last';
-		profileImage = `<img src=/${sender.Image} />`;
-	}
+    let profileImage = '';
+    if (isLast) {
+        liClassName += ' last';
+        profileImage = `<img src=/${sender.Image} />`;
+    }
 
-	let imageContainer = '';
-	if (!isMine) {
-		imageContainer = `<div class="imageContainer">
+    let imageContainer = '';
+    if (!isMine) {
+        imageContainer = `<div class="imageContainer">
 													${profileImage}
 											</div>`;
-	}
+    }
 
-	return `<li class='message ${liClassName}'>
+    return `<li class='message ${liClassName}'>
 							${imageContainer}
               <div class='messageContainer'>
 							${nameElement}
@@ -140,4 +132,15 @@ function createMessageHtml(message, nextMessage, lastSenderId) {
                   </span>
               </div>
           </li>`;
+}
+
+function scrollToBottom(animated) {
+    var container = $('.chatMessages');
+    var scrollHeight = container[0].scrollHeight;
+    console.log(scrollHeight);
+    if (animated) {
+        container.animate({ scrollTop: scrollHeight }, 'slow');
+    } else {
+        container.scrollTop(scrollHeight);
+    }
 }
